c605939131501a5e7fc4ab2eee59618fe8bcbc79
astaabacate <astaabacate@gmail.com>
2026-08-23 01:30:50 -0300

    Add missing: index.html, vite.config.ts, manifest.json and .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    strictPort: false,
-    host: true
+    host: true,
   },
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser'
+    minify: 'terser',
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
 })
